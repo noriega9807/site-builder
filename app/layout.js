dde998bc908294c12fde6381b21504a683aad5ad
Erick Noriega <noriega0893@gmail.com>
2023-03-04 01:13:25 -0600

    add dependencies and providers

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
+import { Providers } from './providers'
+
 import '@/styles/globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
