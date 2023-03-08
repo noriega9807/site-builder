40876d3048046367a26c3b36e5cbd6324e4d19ed
Erick Noriega <noriega0893@gmail.com>
2023-03-07 22:08:06 -0600

    move components

---
+++ styles/alias/Button.js
@@ -0,0 +1,26 @@
+export const ButtonTheme = {
+  primary: 'btn-primary',
+  secondary: 'btn-secondary',
+  accent: 'btn-accent',
+  ghost: 'btn-ghost',
+  link: 'btn-link',
+  info: 'btn-info',
+  success: 'btn-success',
+  warning: 'btn-warning',
+  error: 'btn-error',
+  glass: 'glass',
+}
+
+export const ButtonSize = {
+  large: 'btn-lg',
+  medium: 'btn-md',
+  small: 'btn-sm',
+  extraSmall: 'btn-xs',
+  wide: 'btn-wide',
+  block: 'btn-block',
+}
+
+export const ButtonForm = {
+  square: 'btn-square',
+  circle: 'btn-circle',
+}
